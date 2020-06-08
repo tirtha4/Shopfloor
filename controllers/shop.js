8624c29082cde6b63c596dc60351a1f8f6a10d53
tirtha <tsark99@gmail.com>
2020-06-09 00:00:00 +0530

    Adding product details template  and cart button

--- controllers/shop.js
+++ controllers/shop.js
@@ -14,9 +14,12 @@ exports.getProduct = (req,res,next)=>{
 
   const prodId = req.params.productId;
   Product.findById(prodId,product=>{
-    console.log("The product:", product);
+    res.render('shop/product-detail',{
+      product: product,
+      pageTitle: product.title,
+      path: '/products'
+    })
   })
-  res.redirect('/');
 }
 
 exports.getIndex = (req, res, next) => {
